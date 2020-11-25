7e241eedf275dba9de98ab6ce84d4ed9f518294b
HowToDisappear <60031509+HowToDisappear@users.noreply.github.com>
2020-11-25 11:38:10 +0300

    turned debug on

--- blueprint/settings.py
+++ blueprint/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'y9+7(fyu#ch)pnz-@=qm)(&p9kr$t^vpe!t*)gb+ip4oq$l^6e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'blueprint999.herokuapp.com',
